d3abd64ff836d7d133ee4c65df029335a1114b8e
utran0612 <utran@conncoll.edu>
2023-08-15 21:55:59 -0400

    built Login and GenerateAllTokens functions, completed GetUser function

--- controllers/userController.go
+++ controllers/userController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -9,10 +10,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/golang-jwt-project/database"
-	helper "github.com/golang-jwt-project/helpers"
+	helpers "github.com/golang-jwt-project/helpers"
 	models "github.com/golang-jwt-project/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
@@ -20,7 +23,16 @@ var validate = validator.New()
 
 func HashPassword()
 
-func VerifyPassword()
+func VerifyPassword(userPassword string, providePassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(userPassword), []byte(providePassword))
+
+	if err != nil {
+		msg := fmt.Sprintf("password is incorrect")
+		return false, msg
+	}
+
+	return true, ""
+}
 
 func Signup() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -59,15 +71,57 @@ func Signup() gin.HandlerFunc {
 
 		user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 		user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.User_id = user.ID.Hex()
+		token, refreshToken, _ := helpers.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, *user.User_type, *&user.User_id)
+		user.Token = &token
+		user.Refresh_token = &refreshToken
+
+		//add user to DB
+		resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
+		if insertErr != nil {
+			msg := fmt.Sprintf("User item was not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, resultInsertionNumber)
 
 	}
 }
 
-func Login()
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		var foundUser models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+		defer cancel()
+
+		if !passwordIsValid {
+			c.JSON(http.StatusBadRequest, gin.H{"error": msg})
+		}
+	}
+
+}
 
 // only admins can access to these APIs
 func GetUsers() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
 		// userId := c.Param("user_id")
 
 		// //check if user is an admin
@@ -89,7 +143,7 @@ func GetUser() gin.HandlerFunc {
 		userId := c.Param("user_id")
 
 		//check if user is an admin
-		if err := helper.MatchUserTypeToUid(c, userId); err != nil {
+		if err := helpers.MatchUserTypeToUid(c, userId); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,5 +1,59 @@
 package helpers
 
-func GenerateAllTokens(email string, firstName string, lastName string, userType string, userId string) {
+import (
+	"log"
+	"os"
+	"time"
 
+	"github.com/golang-jwt-project/database"
+	jwt "github.com/golang-jwt/jwt"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type SignedDetails struct {
+	Email      string
+	First_name string
+	Last_name  string
+	Uid        string
+	User_type  string
+	jwt.StandardClaims
+}
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(email string, firstName string, lastName string, userType string, uid string) (signedToken string, signedRefreshToken string, err error) {
+	claims := &SignedDetails{
+		Email:      email,
+		First_name: firstName,
+		Last_name:  lastName,
+		Uid:        uid,
+		User_type:  userType,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshCalims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(200)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+
+	if err != nil {
+		log.Panic("Error creating token:", err)
+		return
+	}
+
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshCalims).SignedString([]byte(SECRET_KEY))
+
+	if err != nil {
+		log.Panic("Error creating refersh token", err)
+		return
+	}
+
+	return token, refreshToken, err
 }
